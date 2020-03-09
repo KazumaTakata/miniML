d8b9f8be0bc749fd45de3ff13c87a7978f313306
Kazuma Takata <neilbuzztakata@gmail.com>
2020-03-09 20:03:07 +0900

    add more test

--- lib/lex.ml
+++ lib/lex.ml
@@ -94,11 +94,6 @@ type token = { typeOfToken : tokenType; literal : string }
 
 type lexer = { input : string; position : int; ch : char; if_end : bool }
 
-type tokenAndLexer = { token : token option; lexer : lexer }
-
-let newTokenAndLexer (tok : token option) (lex : lexer) : tokenAndLexer =
-  { token = tok; lexer = lex }
-
 let isLetter = function 'a' .. 'z' | 'A' .. 'Z' | '_' -> true | _ -> false
 
 let isDigit = function '0' .. '9' -> true | _ -> false
@@ -128,21 +123,23 @@ let rec skipWhitespace (lex : lexer) : lexer =
   if lex.if_end then lex
   else
     match lex.ch with
-    | ' ' | '\t' | '\n' | '\r' -> skipWhitespace lex
+    | ' ' | '\t' | '\n' | '\r' -> skipWhitespace (advanceLex lex)
     | _ -> lex
 
 let rec readUntil (lex : lexer) (fn : char -> bool) : lexer =
-  if fn lex.ch then readUntil lex fn else lex
+  if lex.if_end then lex
+  else if fn lex.ch then readUntil (advanceLex lex) fn
+  else lex
 
-let readUntilWithCondition (lex : lexer) (fn : char -> bool) : string * lexer =
+let getNumberOrString (lex : lexer) (fn : char -> bool) : string * lexer =
   let pos = lex.position in
   let newlex = readUntil lex fn in
   let substring = String.sub newlex.input ~pos ~len:(newlex.position - pos) in
   (substring, newlex)
 
-let nextToken (lex : lexer) : tokenAndLexer =
+let nextToken (lex : lexer) : token option * lexer =
   let newlex = skipWhitespace lex in
-  if newlex.if_end then newTokenAndLexer None newlex
+  if newlex.if_end then (None, newlex)
   else
     match newlex.ch with
     | '=' | ';' | ':' | '(' | ')' | ',' | '+' | '>' | '<' | '*' | '{' | '}'
@@ -153,21 +150,19 @@ let nextToken (lex : lexer) : tokenAndLexer =
             literal = Char.escaped newlex.ch;
           }
         in
-        newTokenAndLexer (Some token) (advanceLex newlex)
+        (Some token, advanceLex newlex)
     | _ ->
         if isLetter newlex.ch then
-          let token_string, lexer =
-            readUntilWithCondition newlex isLetterOrDigit
-          in
+          let token_string, lexer = getNumberOrString newlex isLetterOrDigit in
           let tokentype = lookupIdent token_string in
           let token =
             Some { typeOfToken = tokentype; literal = token_string }
           in
-          newTokenAndLexer token lexer
+          (token, lexer)
         else
-          let token_string, lexer = readUntilWithCondition newlex isDigit in
+          let token_string, lexer = getNumberOrString newlex isDigit in
           let token = Some { typeOfToken = INT; literal = token_string } in
-          newTokenAndLexer token lexer
+          (token, lexer)
 
 let newLexer input_code =
   { input = input_code; position = 0; ch = input_code.[0]; if_end = false }
--- test/lex_test.ml
+++ test/lex_test.ml
@@ -46,6 +46,20 @@ let create_lex ((position : int), (if_end : bool), (input : string)) : Lex.lexer
     =
   { input; position; ch = input.[position]; if_end }
 
+let lex_to_string (lexer : Lex.lexer) =
+  String.concat ""
+    [
+      "{";
+      lexer.input;
+      ", ";
+      string_of_int lexer.position;
+      ", ";
+      String.make 1 lexer.ch;
+      ", ";
+      Bool.to_string lexer.if_end;
+      "}";
+    ]
+
 let test_ifLexEnd (input : string) =
   [
     ( "test ifLexInRange1" >:: fun _ ->
@@ -63,16 +77,47 @@ let test_advanceLex input =
     ( "test advanceLex1" >:: fun _ ->
       assert_equal
         (create_lex (1, false, input))
-        (Lex.advanceLex (create_lex (0, false, input))) );
+        (Lex.advanceLex (create_lex (0, false, input)))
+        ~printer:lex_to_string );
     ( "test advanceLex2" >:: fun _ ->
       assert_equal
         (create_lex (String.length input - 1, true, input))
-        (Lex.advanceLex (create_lex (String.length input - 1, false, input))) );
+        (Lex.advanceLex (create_lex (String.length input - 1, false, input)))
+        ~printer:lex_to_string );
+  ]
+
+let test_skipWhitespace input =
+  [
+    ( "test skipWhitespace1" >:: fun _ ->
+      assert_equal
+        (create_lex (1, false, input))
+        (Lex.skipWhitespace (create_lex (0, false, input)))
+        ~printer:lex_to_string );
+  ]
+
+let test_readUntil input =
+  [
+    ( "test readUntil1" >:: fun _ ->
+      assert_equal
+        (create_lex (3, false, input))
+        (Lex.readUntil (create_lex (0, false, input)) Lex.isDigit)
+        ~printer:lex_to_string );
+  ]
+
+let test_getNumberOrString input =
+  [
+    ( "test getNumberOrString" >:: fun _ ->
+      assert_equal
+        ("aa", create_lex (3, false, input))
+        (Lex.getNumberOrString (create_lex (0, false, input)) Lex.isDigit) );
   ]
 
 let tests =
   "test suite for Lex"
   >::: test_CharToTokenType @ test_lookupIdent @ test_isLetter @ test_isDigit
        @ test_isLetterOrDigit @ test_ifLexEnd "(3+3)" @ test_advanceLex "(3+3)"
+       @ test_skipWhitespace " (3+3)"
+       @ test_readUntil "332ij"
+       @ test_getNumberOrString "332ij"
 
 let _ = run_test_tt_main tests
